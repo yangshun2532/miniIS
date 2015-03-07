25166d4290702b9e6d9f13a798a388ccd081ab3f
yangshun2532 <792454122@qq.com>
2015-03-07 16:36:03 +0800

    fix a bug of flan matching

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 
 #define OBJECT_PATH "G:\\BaiduYunDownload\\contest_data\\contest_data\\clothes\\clothes_source\\clothes_250001.jpg"
 #define CLOTH_DIR "G:\\BaiduYunDownload\\contest_data\\contest_data\\clothes\\clothes_image\\" //图像集所在文件夹
-#define CLOTH_AMOUNT 100000    //总文件数
+#define CLOTH_AMOUNT 150000    //总文件数
 #define CLOTH_FIRST_NUMBER 100001  //第一个文件编号
 #define THRESHOLD 400
 
@@ -28,6 +28,9 @@ int main()
     std::vector<KeyPoint> objectKeypoints;
     SurfFeatureDetector detector(minHessian);
     detector.detect(objectImg,objectKeypoints);
+     SurfDescriptorExtractor extractor;
+     Mat descriptors_1;
+     extractor.compute(objectImg,objectKeypoints,descriptors_1);
 
     for(i=1;i<=CLOTH_AMOUNT;i++)
     {
@@ -40,10 +43,11 @@ int main()
         detector.detect(img,keypoints);
 
         //to compare
-        SurfDescriptorExtractor extractor;
-        Mat descriptors_1,descriptors_2;
-        extractor.compute(objectImg,objectKeypoints,descriptors_1);
+        Mat descriptors_2;
         extractor.compute(img,keypoints,descriptors_2);
+
+        if(descriptors_2.empty()) //若果一个特征点都没有是不能进行flan匹配的
+            continue;
 
         FlannBasedMatcher matcher;
         std::vector<DMatch>matches;
@@ -74,20 +78,36 @@ int main()
             best=good_matches.size();
             ans=i;
         }
-        printf("%d\n特征点匹配个数：%d\n最佳匹配是第 %d 张图片（%d）\n",i,good_matches.size(),ans,best);
+        //printf("%d\n特征点匹配个数：%d\n最佳匹配是第 %d 张图片（%d）\n",i,good_matches.size(),ans,best);
 
         //draw the good matches
-        Mat img_matches;
+        /*Mat img_matches;
         drawMatches(objectImg,objectKeypoints,img,keypoints,good_matches,img_matches,
                     Scalar::all(-1),Scalar::all(-1),vector<char>(),DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
         imshow("Good Matches",img_matches);
-        waitKey(0);
+        waitKey(0);*/
     }
+    //将goodmatch信息写入文件
+    FILE* fp=fopen("G:\\goodmatch.txt","a+");
+    if(fp==NULL)
+        return -1;
+    for(i=1;i<=CLOTH_AMOUNT;i++)
+    {
+        fprintf(fp,"%u\n",goodMatch[i]);
+    }
+    fclose(fp);
+    //将ans信息写入文件
+    FILE* fp_1=fopen("G:\\ans.txt","a+");
+    if(fp==NULL)
+        return -1;
+    printf("The best match is NO:%d\n",ans);
+    fclose(fp_1);
+
     return 0;
 }
 
 
-int mySurf(Mat* img)
+int i(Mat* img)
 {
       //-- Step 1: Detect the keypoints using SURF Detector
       int minHessian = THRESHOLD;
